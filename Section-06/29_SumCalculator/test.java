abc14451cbda4d726eac578dc999ce6f3aafd94b
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-10 01:38:32 -0400

    Update test.java

--- Section-06/29_SumCalculator/test.java
+++ Section-06/29_SumCalculator/test.java
@@ -1,1 +1,18 @@
+public class Test {
+    public static void main(String[] args) {
 
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+
+        System.out.println("Addition: " + calculator.getAdditionResult());
+        System.out.println("Subtraction: " + calculator.getSubtractionResult());
+
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+
+        System.out.println("Multiplication: " + calculator.getMultiplicationResult());
+        System.out.println("Division: " + calculator.getDivisionResult());
+    }
+}
