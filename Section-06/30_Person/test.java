30ddcdcdb1fcd87ca268f63e71fe7bb5c99b5944
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-10 02:29:01 -0400

    Update test.java

--- Section-06/30_Person/test.java
+++ Section-06/30_Person/test.java
@@ -1,1 +1,23 @@
+public class Test {
+    public static void main(String[] args) {
 
+        Person person = new Person();
+
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+
+        System.out.println("Full name = " + person.getFullName());
+        System.out.println("Is teen = " + person.isTeen());
+
+        person.setFirstName("John");
+        person.setAge(18);
+
+        System.out.println("Full name = " + person.getFullName());
+        System.out.println("Is teen = " + person.isTeen());
+
+        person.setLastName("Smith");
+
+        System.out.println("Full name = " + person.getFullName());
+    }
+}
