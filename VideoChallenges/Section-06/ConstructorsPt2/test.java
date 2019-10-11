183ae896ff961aee558915c4b9d4193289f1ce85
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-11 16:58:56 -0400

    Update test.java

--- VideoChallenges/Section-06/ConstructorsPt2/test.java
+++ VideoChallenges/Section-06/ConstructorsPt2/test.java
@@ -1,1 +1,21 @@
+public class Test {
+    public static void main(String[] args) {
 
+        VipCustomer Jazmin = new VipCustomer("Jazmin", 1000);
+        VipCustomer Joi = new VipCustomer("Joi", 2000, "joi@email.com");
+        VipCustomer Ryan = new VipCustomer();
+
+        System.out.println(Jazmin.getName());
+        System.out.println(Jazmin.getCreditLimit());
+        System.out.println(Jazmin.getEmail());
+        System.out.println();
+        System.out.println(Joi.getName());
+        System.out.println(Joi.getCreditLimit());
+        System.out.println(Joi.getEmail());
+        System.out.println();
+        System.out.println(Ryan.getName());
+        System.out.println(Ryan.getCreditLimit());
+        System.out.println(Ryan.getEmail());
+
+    }
+}
