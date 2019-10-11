32e29bd14a74a93186e7241dc93d143f7db495d0
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-11 18:31:35 -0400

    Create Point.java

---
+++ Section-06/Point/Point.java
@@ -0,0 +1,42 @@
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt((0 - x) * (0 - x) + (0 - y) * (0 - y));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt((point.getX() - x) * (point.getX() - x) + (point.getY() - y) * (point.getY() - y));
+    }
+}
