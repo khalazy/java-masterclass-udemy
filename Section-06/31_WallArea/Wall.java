140ecf161628e6bf7ea60bea2ae8b463800de7bd
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-11 17:37:07 -0400

    Update Wall.java

--- Section-06/31_WallArea/Wall.java
+++ Section-06/31_WallArea/Wall.java
@@ -1,1 +1,48 @@
+public class Wall {
 
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(0, 0);
+    }
+
+    public Wall(double width, double height) {
+        if(width < 0) {
+            width = 0;
+        }
+        this.width = width;
+
+        if(height < 0) {
+            height = 0;
+        }
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width < 0) {
+            width = 0;
+        }
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if(height < 0) {
+            height = 0;
+        }
+        this.height = height;
+
+    }
+
+    public double getArea() {
+        return height * width;
+    }
+}
