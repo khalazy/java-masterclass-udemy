26670befa199166bc6797603fb62898521038705
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-11 18:45:10 -0400

    Create Carpet.java

---
+++ Section-06/33_CarpetCostCalculator/Carpet.java
@@ -0,0 +1,15 @@
+public class Carpet {
+
+    private double cost;
+
+    public Carpet(double cost) {
+        if(cost < 0) {
+            cost = 0;
+        }
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
