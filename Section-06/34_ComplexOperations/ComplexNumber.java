c2a7a3c6fcd4d2b46db208418fa76cc70d4b2395
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-11 19:13:15 -0400

    Create ComplexNumber.java

---
+++ Section-06/34_ComplexOperations/ComplexNumber.java
@@ -0,0 +1,38 @@
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        real += complexNumber.getReal();
+        imaginary += complexNumber.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber) {
+        real -= complexNumber.getReal();
+        imaginary -= complexNumber.getImaginary();
+    }
+}
