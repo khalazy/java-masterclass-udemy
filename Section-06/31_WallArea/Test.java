cce306dc679c1cbd3df34c656b93f884b1241f98
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-11 17:37:21 -0400

    Update Test.java

--- Section-06/31_WallArea/Test.java
+++ Section-06/31_WallArea/Test.java
@@ -1,1 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
 
+        Wall wall = new Wall(5, 4);
+
+        System.out.println("area = " + wall.getArea());
+
+        wall.setHeight(-1.25);
+        System.out.println("width = " + wall.getWidth());
+        System.out.println("height = " + wall.getHeight());
+        System.out.println("area = " + wall.getArea());
+
+        wall.setWidth(-1.25);
+        System.out.println("width = " + wall.getWidth());
+    }
+}
