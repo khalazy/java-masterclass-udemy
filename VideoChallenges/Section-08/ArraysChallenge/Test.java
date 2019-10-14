a56d17314493b13a1cff93cb4c91207546c97793
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-14 10:53:24 -0400

    Update Test.java

--- VideoChallenges/Section-08/ArraysChallenge/Test.java
+++ VideoChallenges/Section-08/ArraysChallenge/Test.java
@@ -6,6 +6,7 @@ public class Test {
 
     public static void main(String[] args) {
         int[] array = getArray(5);
+        System.out.println("***UNSORTED ARRAY***");
         printArray(array);
         sortArray(array);
         System.out.println("***SORTED ARRAY***");
@@ -30,12 +31,17 @@ public class Test {
 
     public static void sortArray(int[] array) {
         int temp;
+        boolean flag = true;
 
-        for(int i = 0; i < array.length - 1; i++) {
-            if(array[i] < array[i + 1]) {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
+        while(flag) {
+            flag = false;
+            for(int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    flag = true;
+                }
             }
         }
     }
