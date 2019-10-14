380370c0a5535bb8bac4b25f7eeba4c4476cd399
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-10-14 10:41:55 -0400

    Create Test.java

---
+++ VideoChallenges/Section-08/ArraysChallenge/Test.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Test {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] array = getArray(5);
+        printArray(array);
+        sortArray(array);
+        System.out.println("***SORTED ARRAY***");
+        printArray(array);
+    }
+
+    public static int[] getArray(int number) {
+        System.out.println("Enter digits: \r");
+        int[] newArray = new int[number];
+
+        for(int i = 0; i < newArray.length; i++) {
+            newArray[i] = scanner.nextInt();
+        }
+        return newArray;
+    }
+
+    public static void printArray(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void sortArray(int[] array) {
+        int temp;
+
+        for(int i = 0; i < array.length - 1; i++) {
+            if(array[i] < array[i + 1]) {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
