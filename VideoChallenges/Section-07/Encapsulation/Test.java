37c9e400abf4b9b9d6de79ffc84a4afb442e23e3
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-11-04 16:15:01 -0500

    Update Test.java

--- VideoChallenges/Section-07/Encapsulation/Test.java
+++ VideoChallenges/Section-07/Encapsulation/Test.java
@@ -1,1 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
 
+        Printer printer = new Printer(100, 0, true);
+        System.out.println("Toner level is at: " + printer.getTonerLevel() + "%.");
+        System.out.println(printer.getPagesPrinted() + " pages have been printed.");
+        System.out.println("Is this printer a duplex printer? " + printer.isDuplexPrinter());
+
+        System.out.println("New toner amount: " + printer.fillToner(20));
+        System.out.println("Total pages printed: " + printer.printPages(5));
+    }
+}
