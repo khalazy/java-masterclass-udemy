f85f526d348629d36dc1153905fc2801777404d6
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-11-04 13:55:49 -0500

    Create Television.java

---
+++ VideoChallenges/Section-07/Composition/Television.java
@@ -0,0 +1,17 @@
+class Television {
+    private int size;
+    private int resolution;
+
+    Television(int size, int resolution) {
+        this.size = size;
+        this.resolution = resolution;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int getResolution() {
+        return resolution;
+    }
+}
