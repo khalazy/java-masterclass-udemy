fd4e662079971d902cfc5f4e0883eb9c1e0c1cff
Jazmin Padron <25379110+khalazy@users.noreply.github.com>
2019-11-04 13:55:33 -0500

    Create Bed.java

---
+++ VideoChallenges/Section-07/Composition/Bed.java
@@ -0,0 +1,17 @@
+class Bed {
+    private String size;
+    private int pillows;
+
+    Bed(String size, int pillows) {
+        this.size = size;
+        this.pillows = pillows;
+    }
+
+    String getSize() {
+        return size;
+    }
+
+    int getPillows() {
+        return pillows;
+    }
+}
